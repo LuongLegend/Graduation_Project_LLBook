632f03bb12fa24cac3e437541bc7f54862de6979
LuongLegend <luongprolv99@gmail.com>
2021-01-28 10:27:10 +0700

    done basic header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,25 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSearch } from '@fortawesome/free-solid-svg-icons'
-import './Header.css';
-import './abc.scss';
+import { faSearch, faCartArrowDown, faHeart } from '@fortawesome/free-solid-svg-icons'
+import './Header.scss';
 import logo from '../assets/img/logo.jpg';
 export default function Header() {
     return (
-        <header>
-            <img className='logo' src={logo} alt='LLBook store'/>
-            <span className='llbook'>LL BOOK</span>
+        <header className='header'>
+            <div className='header__logo'>
+                <img className='header__logo-img' src={logo} alt='LLBook store' />
+                <span className='header__logo-llbook'>LL BOOK</span>
+            </div>
             <form className='form-group'>
                 <input className='form-field' type='search' placeholder='Tìm kiếm tựa sách, tác giả' />
-                <span><FontAwesomeIcon icon={faSearch} className='icon-search' /></span>
+                <span><FontAwesomeIcon icon={faSearch}  /></span>
             </form>
-            <p>12314</p>
+            <div>
+                <FontAwesomeIcon icon={faCartArrowDown} className='header-icon color-blueviolet' />
+                <FontAwesomeIcon icon={faHeart} className='header-icon' />
+                <span>Đăng nhập | </span>
+                <span>Đăng ký</span>
+            </div>
         </header>
     )
 }
